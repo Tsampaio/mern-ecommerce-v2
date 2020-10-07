87d3b1c1128f5a57dc3177d8c2e20bce56442349
Telmo Sampaio <telmoasampaio@gmail.com>
2020-10-07 23:24:11 +0100

    Modeling the data

---
+++ backend/data/user.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+	{
+		name: "Admin User",
+		email: 'admin@email.com',
+		password: bcrypt.hashSync('123456', 10),
+		isAdmin: true
+	},
+	{
+		name: "John Doe",
+		email: 'john@email.com',
+		password: bcrypt.hashSync('123456', 10)
+	},
+	{
+		name: "Rachel Terry",
+		email: 'rachel@email.com',
+		password: bcrypt.hashSync('123456', 10)
+	},
+];
+
+export default users;
