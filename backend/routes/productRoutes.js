1fe7d6ec732ff02a18e4c8cf4eec138611613d39
Telmo Sampaio <telmoasampaio@gmail.com>
2020-10-13 23:24:12 +0100

    Custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,7 +3,6 @@ import asyncHandler from 'express-async-handler';
 const router = express.Router();
 import Product from '../models/productModel.js';
 
-
 //@desc     Fetch All Products
 //@route    Get /api/products
 //@access   Public
@@ -11,8 +10,6 @@ router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products)
 }));
-
-
 
 //@desc     Fetch Single Product
 //@route    Get /api/products/:id
@@ -23,7 +20,8 @@ router.get('/:id', asyncHandler( async (req, res) => {
     if(product) {
         res.json(product)
     } else {
-        res.status(404).json({ message: 'Product not found' })
+        res.status(404)
+        throw new Error('Product not found');
     }
 
 }));
