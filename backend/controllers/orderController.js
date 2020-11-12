87d02f12f4954984c60852751bed1c32abaa6d3c
Telmo Sampaio <telmoasampaio@gmail.com>
2020-11-12 18:38:04 +0000

    Order Controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+import asyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+
+//@desc     Create New Order
+//@route    Post /api/orders
+//@access   Public
+const addOrderItems = asyncHandler(async (req, res) => {
+  const { 
+    orderItems, 
+    shippingAddress, 
+    paymentMethod, 
+    itemsPrice, 
+    taxPrice, 
+    shippingPrice, 
+    totalPrice } = req.body;
+
+    if(orderItems && orderItems.length === 0) {
+      res.status(400)
+      throw new Error('No order items');
+      return
+    } else {
+      const order = new Order({
+        shippingAddress,
+        user: req.user._id,
+        paymentMethod, 
+        itemsPrice, 
+        taxPrice, 
+        shippingPrice, 
+        totalPrice
+      });
+
+      const createOrder = await order.save();
+
+      res.status(201).json(createOrder);
+    }
+});
+
+export { addOrderItems }
