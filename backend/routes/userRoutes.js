53eb7b7e1410c1eacfebae8d0cf04fb5ca61e2f7
Telmo Sampaio <telmoasampaio@gmail.com>
2021-01-02 17:19:53 +0000

    Admin user delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,13 @@
-import express, { Router } from 'express';
+import express from 'express';
 const router = express.Router();
-import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers } from '../controllers/userController.js';
+import { 
+  authUser, 
+  registerUser, 
+  getUserProfile, 
+  updateUserProfile, 
+  getUsers,
+  deleteUser
+ } from '../controllers/userController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser).get(protect, admin, getUsers);
@@ -8,5 +15,6 @@ router.post('/login', authUser);
 router.route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUser);
 
 export default router;
